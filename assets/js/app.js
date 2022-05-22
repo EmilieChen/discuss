071ad5a35af3e21acced1b577ca232d42bce4ff2
EmilieChen <emiliechen100@gmail.com>
2022-05-22 11:03:22 +0200

    create comment in real-time

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ import "../css/app.css"
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
-import socket from "./socket"
+import "./socket"
 
 import topbar from "../vendor/topbar"
 // Show progress bar on live navigation and form submits
